0db50c33ecc4bc05173ada40a5238d9fd8bc9334
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-09-28 06:27:02 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-
+import Header from "./Components/Header";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Header/>
+  </div>;
 }
 
 export default App;
